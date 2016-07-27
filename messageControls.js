611a865236d5e3907b3a635cf2a84a7c449a21b9
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-27 14:17:48 -0500

    Initial files for Modular Code #2

---
+++ messageControls.js
@@ -0,0 +1,17 @@
+// Modular Code
+// 2.
+//
+// One IIFE should contain a function
+// that accepts an element id, and the
+// user message, and then add the user's
+// message - along with the delete button
+// to the specified parent element.
+//
+// Each message should be stored in a
+// private array in this IIFE. This IIFE
+// should also expose a function to read
+// all messages, and delete a single message.
+
+var messageControls = (function controlsModule() {
+
+})();
