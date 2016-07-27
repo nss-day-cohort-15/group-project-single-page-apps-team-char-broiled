6b43b2d3223f6862bdb47279962ec35802a87a06
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-27 15:14:29 -0500

    Merged messageControls and master

--- loadmessages.js
+++ loadmessages.js
@@ -1,13 +1,13 @@
 var Chatty = (function messageMoudle(chatty = {}) {
   messages = [];
 
-  chatty.getSavedMessages = function getMessages(callbackFunction) {
+  chatty.getSavedMessages = function getMessages(callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'messages.json');
     xhr.addEventListener('load', function onLoad() {
       messages = JSON.parse(xhr.responseText);
       console.info('Success: Loaded messages from messages.json.');
-      callbackFunction(messages);
+      callback(messages);
     });
     xhr.send();
   }
