16b6506e9b6a1a974dcc437c84b6254336877958
Jamie Duke <jamsduke@gmail.com>
2016-07-28 11:21:47 -0500

    added toggle to dark theme checkbox

--- events.js
+++ events.js
@@ -6,3 +6,50 @@ function enter(e) {
 }
 
 document.addEventListener("keydown", enter);
+
+
+/*Checkbox Color Changes*/
+
+// $(function(){
+//     $('input[type="checkbox"]').click(function(){
+//         if(this.checked)
+//             $(this).closest('body').addClass('dark');
+//         else
+//             $(this).closest('body').removeClass('dark');
+//     });
+// });​
+
+var colorChange = document.getElementsByClassName("light")
+
+function themeColor() {
+  if (document.getElementById("theme").checked){
+    for (var i = 0; i < colorChange.length; i++){
+      colorChange[i].classList.toggle("dark")
+    }
+  }
+  else{
+    for (var i = 0; i < colorChange.length; i++){
+      colorChange[i].classList.toggle("dark")
+  }
+}};
+
+document.getElementById("theme").addEventListener("click", themeColor);
+
+var textChange = document.getElementsByClassName("textToggle")
+
+function textSize() {
+  if (document.getElementById("size").checked){
+    for (var i = 0; i < textChange.length; i++){
+      textChange[i].classList.toggle("textLarge")
+      console.log("click")
+    }
+  }
+  else{
+    for (var i = 0; i < textChange.length; i++){
+      textChange[i].classList.toggle("textLarge")
+      console.log("unclick")
+  }
+}};
+
+document.getElementById("size").addEventListener("click", textSize);
+console.dir(document.getElementById("size"))
