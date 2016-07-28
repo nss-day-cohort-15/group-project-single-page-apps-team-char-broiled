17de28653b01388815c622180aa67bdeb92c1636
Jamie Duke <jamsduke@gmail.com>
2016-07-28 11:45:40 -0500

    fixed enter key event listener

--- events.js
+++ events.js
@@ -2,7 +2,8 @@
 function enter(e) {
   if (13 == e.keyCode) {
     var userInput = document.getElementById("messageArea")
-    Chatty.addMessage(Chatty.getNextId(), userInput.value)
+    Chatty.addMessage(userInput.value)
+    Chatty.addMessageToDom(Chatty.getNextId(), userInput.value)
   }
 }
 
